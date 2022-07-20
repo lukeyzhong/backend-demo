0f6e30be16e08bc84d1edc7317fb99352435cb46
lukeyzhong <lukeyzhong@gmail.com>
2022-07-20 15:08:09 -0500

    update

---
+++ modules.js
@@ -0,0 +1,4 @@
+// xyz targets to the exports from the require files
+const xyz = require('./people');
+
+console.log(xyz);
---
+++ people.js
@@ -0,0 +1,9 @@
+const person = ['adrian', 'cody', 'brian'];
+const ages = ['10', '20', '30'];
+
+console.log(person);
+
+module.exports = {
+    people: person,
+    ages: ages,
+};
