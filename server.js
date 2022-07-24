a7a68edb5d088a4dbdeb41f73de416ddf23b4c53
lukeyzhong <lukeyzhong@gmail.com>
2022-07-24 12:51:54 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    // console.log('request made');
+    console.log(req.url);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/plain');
+    //write things in html
+    res.write('hello, ninjas');
+    res.end();
+
 })
 
 server.listen(3000, 'localhost', () => {
